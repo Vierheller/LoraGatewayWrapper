ecb7cc75b8152e2c62eae33126d1a648ecb763c1
Vierheller <j.vierheller@outlook.de>
2018-01-21 02:16:38 +0100

    Added test stubs for ImageWatcher

---
+++ src/photo/PhotoDirectoryWatcher.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import {PhotoDirectoryWatcher} from "./PhotoDirectoryWatcher";
+
+describe('processFile', ()=>{
+    it('should')
+});
+
+describe('isDownloadFinished', ()=>{
+    before(function() {
+        //TODO add some photos
+    });
+
+    it('shouldReturnTrueIfNewerFileStarted', (done)=>{
+        //TODO define test
+    });
+
+    after(function() {
+        //TODO remove added photos
+    });
+});
